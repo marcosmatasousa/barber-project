996a2add8e6dc95871bcff6dce920c70edf8287f
marcosmatasousa <marcosdamata2000@gmail.com>
2025-03-20 11:53:30 -0300

    feat: Implemented a swagger

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import auth from "./routes/auth";
 import healthcheck from "./routes/healthcheck";
 import booking from "./routes/booking";
 import agenda from "./routes/agenda";
+import swaggerUI from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
 
 const app = express();
 app.use(express.json());
@@ -12,6 +14,7 @@ app.use(auth);
 app.use(healthcheck);
 app.use(booking);
 app.use(agenda);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
